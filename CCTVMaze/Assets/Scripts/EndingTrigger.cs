b223faf4546d875bc3e8f955280a997ec9dc5b5f
BlaerZhang <zhangyueyang0022@126.com>
2023-02-12 16:14:36 -0500

    Added File IO function: Creating txt file on your desktop when you reach the end of the game

--- CCTVMaze/Assets/Scripts/EndingTrigger.cs
+++ CCTVMaze/Assets/Scripts/EndingTrigger.cs
@@ -4,11 +4,48 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using TMPro;
+using System.IO;
 
 public class EndingTrigger : MonoBehaviour
 {
     private TextMeshProUGUI endingText;
+    
     private bool hitten = false;
+
+    private string systemLog =
+        System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Ipconfig Smart Plug B09HPW" + "     Permitted" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Ping 192.168.122.235/24" + "            Permitted" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Permitted" + "\n"
+        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      "+Environment.UserName+", isn't it?" + "\n"
+        +System.DateTime.Now.ToString() + "     👁👁👁👁👁" + "       👁👁👁👁" + "      👁👁👁👁👁👁👁👁👁👁👁" + "     👁👁👁👁"; //content of the txt file
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +68,7 @@ public class EndingTrigger : MonoBehaviour
                 endingText.text = "The End"; //set text in UI
                 endingText.DOFade(0, 0); //set text alpha to 0
                 endingText.DOFade(1, 3); //text fade in
+                File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+"/System Log.txt", systemLog); //create a txt file on desktop
                 hitten = true; //set status to hitten
             }
         }
